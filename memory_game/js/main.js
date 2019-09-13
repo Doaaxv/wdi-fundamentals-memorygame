c50f8c2217800fa8571522125445444d67a737ff
Doaa <Doaa.mh.uni@gmail.com>
2019-09-13 14:54:48 +0300

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("cardOne: "+cardOne);
+console.log("cardTwo: "+cardTwo);
