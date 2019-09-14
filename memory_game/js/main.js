062d6307e73d7e3906707e0738f0e6f11e48f526
Doaa <Doaa.mh.uni@gmail.com>
2019-09-14 12:18:57 +0300

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("cardOne: "+cardOne);
-console.log("cardTwo: "+cardTwo);
+var cards = ["queen","queen","king","king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped "+cardsInPlay);
+
+if(cardsInPlay.length === 2){
+	console.log("cardsInPlay.length: "+ cardsInPlay.length);
+	if(cardsInPlay[0]===cardsInPlay[1]){
+		alert("found a match!");
+	}else{
+		alert("Sorry, try again.");
+	}
+}
+
+
